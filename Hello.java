bd06b21cbed2778c22ed9aac1d7fe38539266943
Thalari Chandana <new_email@example.com>
2026-05-31 13:08:47 +0530

    added java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
     public static void main(String[] args){
         System.ouy.ptintln("Hello");
         System.out.println("chandu");
-      
+      System.out.println("welcome to java programming");
 
 
     }
